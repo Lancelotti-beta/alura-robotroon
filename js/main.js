0cffac6b2ed7128cee735f3899206e02abe26e1e
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-07-02 21:43:59 -0300

    Usando Swiper para troca de Robôs
    utilizanndo SWIPER para fazer a troca dos robos do projeto Robotron 2000

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+
 const botaoUpgrade = document.getElementById('producao');
 botaoUpgrade.value = 'New Upgrade';
 
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    direction: 'vertical',
+    loop: true,
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // }
+});
